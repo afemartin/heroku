d97b9c40f28df6a34e267bca5f685b0d2d195ac3
Andres Martin <andresmartin8787@gmail.com>
2013-06-28 00:13:56 +0200

    add postgres module to insert and select films data

--- web.js
+++ web.js
@@ -1,6 +1,10 @@
 var express = require("express");
 
+var pg = require('pg');
+
 var app = express();
+
+var db = process.env.DATABASE_URL || 'postgres://andres:martin@localhost:5432/postgres';
 
 app.use(express.logger());
 
@@ -8,7 +12,7 @@ app.use(express.logger());
 // Configuration
 ///////////////////////////////////////
 
-app.configure(function(){
+app.configure(function() {
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.bodyParser());
@@ -17,11 +21,14 @@ app.configure(function(){
 	app.use(app.router);
 });
 
-app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
 	app.use(express.errorHandler());
 });
 
@@ -29,8 +36,40 @@ app.configure('production', function(){
 // Routes
 ///////////////////////////////////////
 
+app.get('/install', function(request, response) {
+	pg.connect(db, function(err, client) {
+		var query = client.query('CREATE TABLE IF NOT EXISTS films (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, description VARCHAR(255), image VARCHAR(255), year INTEGER, genre VARCHAR(255), length INTEGER, country VARCHAR(2))');
+	});
+	response.render('install');
+});
+
+app.post('/', function(request, response) {
+	pg.connect(db, function(err, client) {
+		if (request.body.title && request.body.description) {
+			client.query('INSERT INTO films (title, description) VALUES ($1, $2)', [request.body.title, request.body.description]);
+		}
+		var query = client.query('SELECT * FROM films', function(err, result) {
+			response.render('index', {
+				'films': result.rows
+			});
+		});
+		query.on('end', function() {
+			client.end();
+		});
+	});
+});
+
 app.get('/', function(request, response) {
-	response.render('index');
+	pg.connect(db, function(err, client) {
+		var query = client.query('SELECT * FROM films', function(err, result) {
+			response.render('index', {
+				'films': result.rows
+			});
+		});
+		query.on('end', function() {
+			client.end();
+		});
+	});
 });
 
 
@@ -39,5 +78,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("Listening on " + port);
+	console.log("Listening on port " + port);
 });
