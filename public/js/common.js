91adf4718c803e73e82710582f87995dfb24ded9
Andres Martin <andresmartin8787@gmail.com>
2013-07-01 01:54:31 +0200

    add support for freebase queries and use ajax request for create/delete films

--- public/js/common.js
+++ public/js/common.js
@@ -1,1 +1,62 @@
-$(function() {});
+$(function() {
+
+	// Binding events
+	/////////////////////////////////////
+
+	$('#create-film-form').submit(function(e) {
+		e.preventDefault();
+		var filmData = $(this).serialize();
+		createFilm(filmData);
+	});
+
+	$('.js-film-delete').click(function(e) {
+		e.preventDefault();
+		var filmId = {
+			'id': $(this).attr('data-film-id')
+		};
+		deleteFilm(filmId);
+	});
+
+	$('#search-film-form').submit(function(e) {
+		e.preventDefault();
+		var filmData = $(this).serialize();
+		searchFilm(filmData);
+	});
+
+	// Public functions
+	/////////////////////////////////////
+
+	function createFilm(filmData) {
+		$.post("/film/create.json", filmData, createFilmSuccessCallback, "json");
+	}
+
+	function createFilmSuccessCallback(response) {
+		if (response.success) {
+			location.reload();
+		}
+	}
+
+	function deleteFilm(filmId) {
+		$.post("/film/delete.json", filmId, deleteFilmSuccessCallback, "json");
+	}
+
+	function deleteFilmSuccessCallback(response) {
+		if (response.success) {
+			location.reload();
+		}
+	}
+
+	function searchFilm(filmData) {
+		$.post("/film/search.json", filmData, searchFilmSuccessCallback, "json");
+		if ($('#search-film-form + .well').length > 0) {
+			$('#search-film-form + .well').remove();
+		}
+	}
+
+	function searchFilmSuccessCallback(response) {
+		if (response.success && response.data) {
+			$('#search-film-form').after($('<div></div>').addClass('well').text(response.data));
+		}
+	}
+
+});
--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require("express");
 
 var pg = require('pg');
 
+var freebase = require('freebase');
+
 var app = express();
 
 var db = process.env.DATABASE_URL || 'postgres://andres:martin@localhost:5432/postgres';
@@ -39,24 +41,58 @@ app.configure('production', function() {
 app.get('/install', function(request, response) {
 	pg.connect(db, function(err, client) {
 		var query = client.query('CREATE TABLE IF NOT EXISTS films (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, description VARCHAR(255), image VARCHAR(255), year INTEGER, genre VARCHAR(255), length INTEGER, country VARCHAR(2))');
-	});
-	response.render('install');
-});
-
-app.post('/', function(request, response) {
-	pg.connect(db, function(err, client) {
-		if (request.body.title && request.body.description) {
-			client.query('INSERT INTO films (title, description) VALUES ($1, $2)', [request.body.title, request.body.description]);
-		}
-		var query = client.query('SELECT * FROM films', function(err, result) {
-			response.render('index', {
-				'films': result.rows
-			});
-		});
 		query.on('end', function() {
 			client.end();
 		});
 	});
+	response.render('install');
+});
+
+app.post('/film/create.json', function(request, response) {
+	pg.connect(db, function(err, client) {
+		if (request.body.title && request.body.description) {
+			client.query('INSERT INTO films (title, description) VALUES ($1, $2)', [request.body.title, request.body.description]);
+			response.json({
+				'success': true
+			});
+		} else {
+			response.json({
+				'success': false
+			});
+		}
+	});
+});
+
+app.post('/film/delete.json', function(request, response) {
+	pg.connect(db, function(err, client) {
+		if (request.body.id) {
+			client.query('DELETE FROM films WHERE id = $1', [request.body.id]);
+			response.json({
+				'success': true
+			});
+		} else {
+			response.json({
+				'success': false
+			});
+		}
+	});
+});
+
+app.post('/film/search.json', function(request, response) {
+	if (request.body.title) {
+		freebase.description(request.body.title, {
+			type: '/film/film'
+		}, function(result) {
+			response.json({
+				'success': true,
+				'data': result
+			});
+		});
+	} else {
+		response.json({
+			'success': false
+		});
+	}
 });
 
 app.get('/', function(request, response) {
